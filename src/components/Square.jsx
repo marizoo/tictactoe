144aa61d7de96d16f6bed7bb0447843a4f43e9fb
marizoo <marizoo@meow.woof>
2021-10-29 17:53:05 +0700

    added sass

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
 const Square = ({value}) => {
 
    
     return (
-        <button type="button">
+        <button type="button" classname="square">
             { value }
         </button>
     )
